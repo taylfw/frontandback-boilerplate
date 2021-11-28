6a38df2139d9fa3b04c89fce2c28708b9e498ca1
Frank Taylor <frank.w.taylor88@gmail.com>
2021-11-27 20:32:58 -0500

    first commit

--- db/index.js
+++ db/index.js
@@ -1,8 +1,14 @@
 // Connect to DB
 const { Client } = require('pg');
 const DB_NAME = 'change-this-name'
-const DB_URL = process.env.DATABASE_URL || `postgres://${ DB_NAME }`;
-const client = new Client(DB_URL);
+const DB_URL = process.env.DATABASE_URL || `postgres://postgres@localhost:5432/${DB_NAME}`;
+const client = new Client({
+  connectionString: DB_URL,
+ssl:
+    process.env.NODE_ENV === "production"
+      ? { rejectUnauthorized: false }
+      : undefined,
+});
 
 // database methods
 
